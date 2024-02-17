d0db9287473a475e95413ed2fc382b9fd17d2905
Mayko Kioschi <mkioschi@gmail.com>
2024-02-17 18:09:03 -0300

    Configures colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="p-8">
+        <div className="grid grid-cols-5 gap-4 rounded-sm border border-card-border bg-card p-8 shadow-card">
+          <button className="rounded-md bg-primary p-2 text-primary-foreground hover:bg-primary-active dark:bg-gray-400">
+            Botão de teste
+          </button>
+
+          <button className="rounded-md bg-success p-2 text-success-foreground hover:bg-success-active">
+            Botão de teste
+          </button>
+
+          <button className="rounded-md bg-info p-2 text-info-foreground hover:bg-info-active">
+            Botão de teste
+          </button>
+
+          <button className="rounded-md bg-warning p-2 text-warning-foreground hover:bg-warning-active">
+            Botão de teste
+          </button>
+
+          <button className="rounded-md bg-danger p-2 text-danger-foreground hover:bg-danger-active">
+            Botão de teste
+          </button>
+        </div>
+      </div>
     </>
   )
 }
